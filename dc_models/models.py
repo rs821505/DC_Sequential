cb154d1caf5f38eaa6a3082c415d8fd0bf0a1c80
rs821505 <107643011+rs821505@users.noreply.github.com>
2023-01-12 16:14:04 -0600

    "changed class format"

--- dc_models/models.py
+++ dc_models/models.py
@@ -1,40 +1,64 @@
 import numpy as np
 import pandas as pd
 from scipy import integrate
-from math import pi,atan
+from math import pi, atan
 import ipywidgets as ipw
 import math
 import warnings
-warnings.filterwarnings('ignore')
+
+warnings.filterwarnings("ignore")
 
 
-class model:
-    def __init__(self,**kwargs):
-        """
+class Model:
+    def __init__(self, **kwargs):
+        """Base model class for all ode system models
+
         Parameters
-        _________________       
-        :param state_init:      initial options: x,y or s,i,r or r,l,c
-        :param num_tsteps:       step size
-        :param tend:            stopping time
-        :param true_model:      boolean true if this is ground truth model
-        :param N:               population size for sir
-        :param num_parameters:   number of parameters to estimate
-        :supports:              list of supports for each parameter
-        :param drift_windows:   number of parameter drift (resampling) windows
-        :param assim_windows:   number of time intervals
+        ----------
+        model : str
+            string name of the desired ode model class
+        number_forward_runs : int
+            number of forward simulations to run to produce samples for  dc inversion
+        number_observations: int
+             output of our observation operator (or number of observations/sensors)
+        initial_state : np.ndarray
+            initial options: x,y or s,i,r or r,l,c
+        true_model : bool
+            true or predicted forward model choice
+        total_timesteps : int
+            total number of steps to propogate forward in time
+        number_timesteps : int
+            number of samples to take from true state trajectory
+        number_samples : int
+            number of samples to draw per parameter
+        end_time : int
+            end time of the simulation
+        number_parameters : int
+            number of parameters to estimate
+        lambda_true : list
+            true lambda parameter values to be estimated
+        supports : list
+            supports of the probability distributions for each lambda parameter
+        drift_windows : int
+            number of windows to split simulation into
+        assimilation_windows : int
+            number of assimilation windows for data assimilation
+
         """
 
-        self.__dict__.update((param, value) for param,value in kwargs.items()) 
-        self.times = np.split(np.linspace(0.,self.tend, self.num_tsteps),self.drift_windows)                 
+        self.__dict__.update((param, value) for param, value in kwargs.items())
+
+        self.times = np.split(
+            np.linspace(0.0, self.end_time, self.total_timesteps), self.drift_windows
+        )
         self.states = list()
         self.parameters = list()
-        
-    def _integrate(self, ode_system, x0,time,poi):
-        """
-        """
-        return integrate.odeint(ode_system, x0, time, args = poi)
-    
-    def _get_parameters(self):
+
+    def integrate(self, ode_system, x0, time, poi):
+        """integrate the given ode system forward in time"""
+        return integrate.odeint(ode_system, x0, time, args=poi)
+
+    def get_parameters(self):
         """
         Params:
         ---------------
@@ -60,61 +84,86 @@ class model:
             return tuple(self.lambda_true)
         else:
             sampled_parameters = tuple(
-                [np.random.uniform(*self.supports[i]) for i in range(self.num_parameters)]
+                [
+                    np.random.uniform(*self.supports[i])
+                    for i in range(self.number_parameters)
+                ]
             )
         return sampled_parameters
-    
-    def _save_output(self,state,parameter):
+
+    def save_output(self, state, parameter):
         self.states.append(state)
         self.parameters.append(parameter)
 
-    def _get_outputs(self):
-        return np.asarray(self.states), np.asarray(self.parameters), np.asarray(self.times)
+    def get_outputs(self):
+        return (
+            np.asarray(self.states),
+            np.asarray(self.parameters),
+            np.asarray(self.times),
+        )
+
+    def generate_runs(self):
+
+        for i in range(self.number_forward_runs):
+
+            self.run_model()
+            states, params, sim_times = self.get_outputs()
+
+            if i:
+                runs = np.append(
+                    runs,
+                    states[:, :: (states.shape[1] // self.number_observations), :],
+                    axis=0,
+                )
+                lambdas[i, :, :] = params
+            else:
+                runs = states[:, :: (states.shape[1] // self.number_observations), :]
+                lambdas = np.zeros(
+                    [self.number_forward_runs, params.shape[0], params.shape[1]]
+                )
+                lambdas[0, :, :] = params
+
+        return runs, lambdas
 
 
+class SirModel(Model):
+    """ """
 
-class sir(model):
-    """
-    """
-    
-    def _system(self,states,time,*p):
+    def system(self, states, time, *p):
         """
-        sir state space system     s'=-beta*s*i/N
-                                   i'= beta*s*i/N - gamma*i
+        sir state space system     s'=-beta*s*i/population_size
+                                   i'= beta*s*i/population_size - gamma*i
                                    r'= gamma*i
-                                   
+
         Parameters:
         _____________________
-        :param p[0]:= beta
-        :param p[1]:= gamma
+        :p[0], p[1]:= beta, gamma
         """
-        dx_1dt = -p[0] * states[0] * states[1] / self.N
-        dx_2dt = p[0]* states[0] * \
-                  states[1] / self.N - p[1]  * states[1]
+        dx_1dt = -p[0] * states[0] * states[1] / self.population_size
+        dx_2dt = p[0] * states[0] * states[1] / self.population_size - p[1] * states[1]
         dx_3dt = p[1] * states[1]
         diffs = np.array([dx_1dt, dx_2dt, dx_3dt])
         return diffs
-    
-    def _run_model(self):
-        """
-        """
-        X0 = self.state_init
-        
-        for i in range(self.drift_windows):
-            pargs = self._get_parameters()                 
-            s,i,r = self._integrate(self._system, X0, self.times[i], pargs).T
-            X0 = s[-1],i[-1],r[-1]
-            self._save_output(np.column_stack([s,i,r]),pargs[0]/pargs[1])
 
-class lotka_volterra(model):
-    """
-    """
-    
-    def _system(self,states,time,*p):
+    def run_model(self):
+        """ """
+        # X0 = self.initial_state
+
+        for i in range(self.drift_windows):
+            pargs = self.get_parameters()
+            s, i, r = self.integrate(
+                self.system, self.initial_state, self.times[i], pargs
+            ).T
+            self.initial_state = s[-1], i[-1], r[-1]
+            self.save_output(np.column_stack([s, i, r]), pargs[0] / pargs[1])
+
+
+class LotkaVolterraModel(Model):
+    def system(self, states, time, *p):
         """
         lotka_volterra state-space system:   x'= x(a-by)
                                              y'= y(-d+gx)
-        
+
         Parameters:
         _______________
         :param p[0]:= alpha
@@ -126,54 +175,42 @@ class lotka_volterra(model):
         dx_2dt = states[1] * (-p[2] + p[3] * states[0])
         diffs = np.array([dx_1dt, dx_2dt])
         return diffs
-    
-    def _run_model(self):
-        """
-        """
-        
-        X0 = list(self.state_init)   
+
+    def run_model(self):
+        """ """
+
+        X0 = list(self.initial_state)
 
         for i in range(self.drift_windows):
 
-            pargs = self._get_parameters()
-            res = self._integrate(self._system,X0,self.times[i],pargs)
-            X0 = res[-1,:]
-            self._save_output(res,pargs)
+            pargs = self.get_parameters()
+            res = self.integrate(self.system, X0, self.times[i], pargs)
+            X0 = res[-1, :]
+            self.save_output(res, pargs)
 
 
-class rlc(model):
-    """
-    
-    """
-    
-    def _system(self, states,times,*p):
+class RlcModel(Model):
+    def system(self, states, times, *p):
         """
         rlc state-space system: V0 = Lx" + Rx' +x/C.
-        
+
         Parameters:
         _______________
-        :param p[0]:= r
-        :param p[1]:= l
-        :param p[2]:= c
+        :param p[0], p[1], p[2]:= r, l, c
         """
-        v0=5
-        return [states[1],(v0/p[2])-(p[0]/p[1])*states[1]-states[0]/(p[1]*p[2])]
-    
-    def _run_model(self):
-        """
+        v0 = 5
+        return [
+            states[1],
+            (v0 / p[2]) - (p[0] / p[1]) * states[1] - states[0] / (p[1] * p[2]),
+        ]
 
-        """
-#         pargs = tuple([.2,1,1])  # hard coded for initial testing
+    def run_model(self):
+        """ """
+        #         pargs = tuple([.2,1,1])  # hard coded for initial testing
         for i in range(self.drift_windows):
 
-            X0 =  self.state_init
-            pargs = self._get_parameters()
-
-            res = self._integrate(self._system,X0,self.times[i],pargs)
-            X0 = res[-1,:]
-            self._save_output(res,pargs)
-
-        
-    
-    
-    
+            X0 = self.initial_state
+            pargs = self.get_parameters()
+            res = self.integrate(self.system, X0, self.times[i], pargs)
+            X0 = res[-1, :]
+            self.save_output(res, pargs)
